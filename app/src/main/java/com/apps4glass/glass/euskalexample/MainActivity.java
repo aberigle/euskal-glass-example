191cd9a638ca7a48a77d93114222eba00d5708e6
Alejandro Berzosa <alejandro@apps4glass.com>
2014-07-25 10:25:44 +0200

    Bootstrap android-studio new project

---
+++ app/src/main/java/com/apps4glass/glass/euskalexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.apps4glass.glass.euskalexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.apps4glass.glass.euskalexample.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
