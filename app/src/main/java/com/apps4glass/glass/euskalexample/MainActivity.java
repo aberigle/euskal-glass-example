5929e7c3846ffe4c3de61ed44378dfd6fd9cf306
Alejandro Berzosa <alejandro@apps4glass.com>
2014-07-25 12:15:53 +0200

    Add basic example layout

--- app/src/main/java/com/apps4glass/glass/euskalexample/MainActivity.java
+++ app/src/main/java/com/apps4glass/glass/euskalexample/MainActivity.java
@@ -1,11 +1,14 @@
 package com.apps4glass.glass.euskalexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.apps4glass.glass.euskalexample.R;
 import com.google.android.glass.app.Card;
@@ -19,18 +22,40 @@ public class MainActivity extends Activity {
 
     private List<Card> cards;
     private CardScrollView cardScrollView;
+    private List<ExampleCase> examples;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         cards = new ArrayList<Card>();
+        examples = new ArrayList<ExampleCase>();
 
-        cards.add(new Card(this).setText("First card"));
+        populateExamples();
+
+        for (ExampleCase example : examples) cards.add(new Card(this).setText(example.getText()));
 
         cardScrollView = new CardScrollView(this);
         cardScrollView.setAdapter(new SimpleCardScrollAdapter());
         cardScrollView.activate();
+
+        cardScrollView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int selected, long l) {
+                examples.get(selected).onSelected();
+            }
+        });
         setContentView(cardScrollView);
+    }
+
+    private void populateExamples() {
+
+        examples.add(new ExampleCase("Test Toast") {
+            @Override
+            public void onSelected() {
+                Toast.makeText(getBaseContext(), getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private class SimpleCardScrollAdapter extends CardScrollAdapter {
@@ -47,5 +72,15 @@ public class MainActivity extends Activity {
         @Override
         public int getPosition(Object o) { return cards.indexOf(o); }
 
+    }
+
+    private abstract class ExampleCase {
+        private String text;
+
+        public ExampleCase(String text) { this.text = text; }
+
+        public String getText() { return text; }
+
+        public abstract void onSelected();
     }
 }
