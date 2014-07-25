f6226cbf03f3b5a92c4b519031a21913743de5ee
Alejandro Berzosa <alejandro@apps4glass.com>
2014-07-25 11:13:25 +0200

    add basic scroll adapter

--- app/src/main/java/com/apps4glass/glass/euskalexample/MainActivity.java
+++ app/src/main/java/com/apps4glass/glass/euskalexample/MainActivity.java
@@ -4,12 +4,48 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.apps4glass.glass.euskalexample.R;
+import com.google.android.glass.app.Card;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    private List<Card> cards;
+    private CardScrollView cardScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cards = new ArrayList<Card>();
+
+        cards.add(new Card(this).setText("First card"));
+
+        cardScrollView = new CardScrollView(this);
+        cardScrollView.setAdapter(new SimpleCardScrollAdapter());
+        cardScrollView.activate();
+        setContentView(cardScrollView);
+    }
+
+    private class SimpleCardScrollAdapter extends CardScrollAdapter {
+
+        @Override
+        public int getCount() { return cards.size(); }
+
+        @Override
+        public Object getItem(int i) { return cards.get(i); }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) { return cards.get(i).getView(); }
+
+        @Override
+        public int getPosition(Object o) { return cards.indexOf(o); }
+
     }
 }
